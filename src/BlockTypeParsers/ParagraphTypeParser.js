cee7ecebaa7ccacd607a1329d2e4b659af47384b
Gabriel <961534+GabrielGil@users.noreply.github.com>
2021-09-01 13:19:17 +0200

    feat: Parse paragraph children

--- src/BlockTypeParsers/ParagraphTypeParser.js
+++ src/BlockTypeParsers/ParagraphTypeParser.js
@@ -3,31 +3,36 @@ export function parseParagraphToMarkdown(blocks) {
 }
 
 export function parseMarkdownToParagraph(blocks) {
+  console.log('Blocks', blocks);
   let paragraphData = {};
 
   if (blocks.type === 'paragraph') {
-    blocks.children.forEach((item) => {
-      if (item.type === 'text') {
-        paragraphData = {
-          data: {
-            text: item.value,
-          },
-          type: 'paragraph',
-        };
-      }
-      if (item.type === 'image') {
-        paragraphData = {
-          data: {
-            caption: item.title,
-            stretched: false,
-            url: item.url,
-            withBackground: false,
-            withBorder: false,
-          },
-          type: 'image',
-        };
-      }
-    });
+    paragraphData = {
+      data: {
+        text: blocks.children.reduce((acc, curr) => acc.concat(parsePhrasingContentToParagraph(curr)), ''),
+      },
+      type: 'paragraph'
+    }
   }
   return paragraphData;
+}
+
+
+function parsePhrasingContentToParagraph(content) {
+  let text = '';
+  switch (content.type) {
+    case 'break':
+      text = text.concat('<br>');
+      break;
+    case 'text':
+      text = text.concat(content.value);
+      break;
+    case 'emphasis':
+      text = text.concat(`<i>${ content.children.reduce((acc, cur) => acc.concat(parsePhrasingContentToParagraph(cur)), '') }</i>`);
+      break;
+    case 'strong':
+      text = text.concat(`<b>${ content.children.reduce((acc, cur) => acc.concat(parsePhrasingContentToParagraph(cur)), '') }</b>`);
+      break;
+  }
+  return text;
 }
