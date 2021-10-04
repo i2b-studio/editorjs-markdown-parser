45ea9072c86d135ca3e67cc291c542737fa26639
Gabriel Gil <961534+GabrielGil@users.noreply.github.com>
2021-10-04 16:02:39 +0200

    feat: Paste markdown instead uploading a file

--- src/MarkdownImporter.js
+++ src/MarkdownImporter.js
@@ -6,8 +6,6 @@ import { parseMarkdownToDelimiter } from './BlockTypeParsers/DelimiterTypeParser
 import { parseMarkdownToCode } from './BlockTypeParsers/CodeTypeParser';
 import { parseMarkdownToQuote } from './BlockTypeParsers/QuoteTypeParser';
 
-export const editorData = [];
-
 /**
  * Markdown Import class
  */
@@ -16,8 +14,7 @@ export default class MarkdownImporter {
    * creates the Importer plugin
    * {editorData, api functions} - necessary to interact with the editor
    */
-  constructor({ data, api }) {
-    this.data = data;
+  constructor({ api }) {
     this.api = api;
   }
 
@@ -26,80 +23,60 @@ export default class MarkdownImporter {
    */
   static get toolbox() {
     return {
-      title: 'Import Markdown',
+      title: 'Paste Markdown',
       icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgb(112, 118, 132)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-fileUpload"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>',
     };
   }
 
   /**
-  * @return invinsible file upload form
-  */
+   * @return HTMLElement Empty div. It is replaced after rendering.
+   */
   render() {
-    const doc = document.createElement('input');
-    doc.setAttribute('id', 'file-upload');
-    doc.setAttribute('type', 'file');
-    doc.setAttribute('style', 'display: none');
-    doc.setAttribute('name', 'files[]');
-    doc.setAttribute('onload', this.parseMarkdown());
+    // Ask for markdown data
+    const markdown = document.defaultView.prompt('Markdown text...', '');
 
-    return doc;
+    // Parse into a list of EditorJS blocks
+    const blocks = this.markdownToBlock(markdown);
+    blocks.forEach(({ type, data }) => this.api.blocks.insert(type, data));
+
+    // Delete current block right after
+    setTimeout(() => { this.api.blocks.delete(this.api.blocks.getCurrentBlockIndex() - 1); }, 0);
+
+    return document.createElement('div');
   }
 
   /**
-  * Function which parses markdown file to JSON which correspons the the editor structure
-  * @return Parsed markdown in JSON format
-  */
-  async parseMarkdown() {
-    // empty the array before running the function again
-    editorData.length = 0;
+   * Convert some text in Markdown format to a list of blocks
+   *
+   * @param markdown string Text to parse
+   */
+  markdownToBlock(markdown) {
+    // Blocks with the parsed content
+    const blocks = [];
 
-    const a = {};
-    const data = await this.api.saver.save();
-    a.content = data.blocks;
+    const parsedMarkdown = remark().parse(markdown);
+    // Iterate over the pared remarkjs syntax tree and executing the json parsers
+    parsedMarkdown.children.forEach((item) => {
+      switch (item.type) {
+        case 'heading':
+          return blocks.push(parseMarkdownToHeader(item));
+        case 'paragraph':
+          return blocks.push(parseMarkdownToParagraph(item));
+        case 'list':
+          return blocks.push(parseMarkdownToList(item));
+        case 'thematicBreak':
+          return blocks.push(parseMarkdownToDelimiter());
+        case 'code':
+          return blocks.push(parseMarkdownToCode(item));
+        case 'blockquote':
+          return blocks.push(parseMarkdownToQuote(item));
+        default:
+          break;
+      }
+      return null;
+    });
 
-    const fileUpload = document.getElementById('file-upload');
-
-    fileUpload.onchange = (e) => {
-      const file = e.target.files[0];
-
-      const reader = new FileReader();
-      reader.readAsText(file, 'UTF-8');
-
-      reader.onload = (readerEvent) => {
-        const content = readerEvent.target.result;
-
-        const parsedMarkdown = remark().parse(content);
-        // iterating over the pared remarkjs syntax tree and executing the json parsers
-        parsedMarkdown.children.forEach((item, index) => {
-          switch (item.type) {
-            case 'heading':
-              return editorData.push(parseMarkdownToHeader(item));
-            case 'paragraph':
-              return editorData.push(parseMarkdownToParagraph(item));
-            case 'list':
-              return editorData.push(parseMarkdownToList(item));
-            case 'thematicBreak':
-              return editorData.push(parseMarkdownToDelimiter());
-            case 'code':
-              return editorData.push(parseMarkdownToCode(item));
-            case 'blockquote':
-              return editorData.push(parseMarkdownToQuote(item));
-            default:
-              break;
-          }
-        });
-        // clear the editor
-        this.api.blocks.clear();
-        // render the editor with imported markdown data
-        this.api.blocks.render({
-          blocks: editorData.filter((value) => Object.keys(value).length !== 0), // filter through array and remove empty objects
-        });
-
-        return remark().parse(content);
-      };
-    };
-
-    fileUpload.click();
+    return blocks;
   }
 
   save() {
